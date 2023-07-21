9b774dbf22b549e2b035a3c556e56f78b90ea896
imMAXIMUS <63053441+0xmaximus@users.noreply.github.com>
2023-07-21 18:52:24 +0330

    Create xor.py

---
+++ xor.py
@@ -0,0 +1,16 @@
+import sys
+def xor_content(content, key: int):
+    for i in range(len(content)):
+        content[i] ^= 254
+    return bytearray(content)  
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("USAGE:")
+        print("python3 xor_file.py <KEY> <INPUT_FILENAME> <OUTPUT_FILENAME>")
+        exit(0)
+    with open(sys.argv[2], "rb") as i_file:
+        in_content = bytearray(i_file.read())
+        output = xor_content(in_content, int(sys.argv[1]))
+        with open(sys.argv[3], "wb") as o_file:
+            o_file.write(bytearray(output))
